a12a1ffc804b8d1b458e62e60c88f0b7aa570d7a
Mark <mark@markjwood.com>
2021-08-13 14:29:53 -0700

    Refactoring - completely re-did validation function and test variables. Validation on submit working as expected. TODO: real-time validation.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
-const nameInput = document.getElementById('name');
-const emailInput = document.getElementById('email');
-const ccNumInput = document.getElementById('cc-num');
-const zipInput = document.getElementById('zip');
-const cvvInput = document.getElementById('cvv');
-
 // 'name' input gets focus by default
-nameInput.focus();
+document.getElementById('name').focus();
 
 
 ////
@@ -34,19 +28,19 @@ const colorSelectDiv = document.getElementById('shirt-colors');
 const colorSelector = document.getElementById('color');
 const shirtDesignSelector = document.getElementById('design');
 
-  // Shirt color dropdown hidden until shirt design selected
-colorSelectDiv.style.display = 'none'; // hidden by default
+  // Shirt color dropdown disabled until shirt design selected
+colorSelector.disabled = true; 
 
 shirtDesignSelector.addEventListener('change', design => {
+  colorSelector.disabled = false;
   const designChoice = design.target.value;
-  colorSelectDiv.style.display = 'block';
   let first;
   for (let i = 0; i < colorSelector.children.length; i++) {
     const option = colorSelector.children[i];
     if (option.getAttribute('data-theme') !== designChoice) {
-      option.setAttribute('hidden', '');
+      option.hidden = true;
     } else {
-      option.removeAttribute('hidden');
+      option.hidden = false;
 
       // set first available option as 'selected'
       if (!first) first = i;
@@ -60,18 +54,28 @@ shirtDesignSelector.addEventListener('change', design => {
 
 ////
 // "Register for activities" section
-let total = 0;
+let totalCost = 0;
 const activitiesBox = document.getElementById('activities-box');
 const activitiesCostP = document.getElementById('activities-cost');
+const activityCheckboxes = activitiesBox.querySelectorAll('input[type=checkbox]');
+
+for (let check of activityCheckboxes) {
+  check.addEventListener('focus', () => {
+    check.parentElement.classList.add('focus');
+  });
+  check.addEventListener('blur', () => {
+    check.parentElement.classList.remove('focus');
+  });
+}
 
 activitiesBox.addEventListener('change', e => {
-  const price = Number(e.target.getAttribute('data-cost'));
+  const eventPrice = +e.target.getAttribute('data-cost');
   if (e.target.checked) {
-    total += price;
+    totalCost += eventPrice;
   } else {
-    total -= price;
+    totalCost -= eventPrice;
   };
-  activitiesCostP.innerHTML = `Total: $${total}`;
+  activitiesCostP.innerHTML = `Total: $${totalCost}`;
 });
 // end "Register for activities"
 ////
@@ -84,11 +88,13 @@ const ccDiv = document.getElementById('credit-card');
 const payPalDiv = document.getElementById('paypal');
 const btcDiv = document.getElementById('bitcoin');
 
-payMethod.children[1].selected = true; // "Credit Card" selected by default
-
+  // "Credit Card" selected by default
+payMethod.children[1].selected = true; 
   // Hide other payment options unless selected
 payPalDiv.style.display = 'none';
 btcDiv.style.display = 'none';
+
+  // reference appropriate variables from payment option values
 const payOptions = {
   paypal: 'payPalDiv',
   bitcoin: 'btcDiv',
@@ -110,196 +116,168 @@ payMethod.addEventListener('change', e => {
 
 
 ////
-// form validation functions
-const nameTest = name => {
-  if (name) {
-    return 'valid';
-  }
-}
+// form validation
+const form = document.querySelector('form');
 
-const emailTest = email => {
-  if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
-    return 'valid';
-  }
-  if (!/@{1}/.test(email)) {
-    return 'noAt';
-  }
-  if (!/^\S+@\w{3,}$/.test(email)) {
-    return 'noDomain';
-  }
-  if (!/$\S+@\S+\.[a-z]i{2,}$/.test(email)) {
-    return 'noSuffix';
-  }
-  return 'other';
-}
+const dontTest = ['size', 'design', 'color', 'title', 'payment', 'exp-month', 'exp-year'];
 
-const ccNumTest = num => {
-  if (!Number(num) && Number(num) !== 0) {
-    return 'notNumber';
-  }
-  if (num.length < 13) {
-    return 'tooShort';
-  }
-  if (num.length > 16) {
-    return 'tooLong';
-  }
-  if (Number(num) === 0) {
-    return 'zero';
-  }
-  return 'valid';
-}
-
-const zipTest = num => {
-  if (num.length !== 5) return 'blank';
-  if (Number(num) === 0 && num.length > 0) return 'zero';
-  if (!Number(num) && num.length > 0) return 'notNumber';
-  return 'valid';
-}
-
-const cvvTest = num => {
-  if (num.length !== 3) return 'blank';
-  if (Number(num) === 0 && num.length > 0) return 'zero';
-  if (!Number(num) && num.length > 0) return 'notNumber';
-  return 'valid';
-}
-  
-function validateText(inputId) {
-  // inputId = id of input element to test
-  const value = document.getElementById(inputId).value;
-  const testFuncName = camelify(inputId) + 'Test';
-  if (!value) {
-    return 'blank';
-  } else if (eval(testFuncName)(value) === 'valid') {
-    return 'valid';
-  } else {
-    return eval(testFuncName)(value);
-  }
-}
-
-  // validation event listeners
-const hintMsgs = {
+  // validation tests
+const tests = {
+  name: {
+    blank: [/\w+/i, 'Name cannot be left blank']
+  },
   email: {
-    blank: "Email address cannot be blank",
-    noAt: "Email address must contain '@' symbol",
-    noDomain: "Email address must contain a domain name after the '@', e.g. 'gmail.com'",
-    noSuffix: "Email address must contain domain suffix, e.g. '.com'",
-    other: "Email address must be in the format: name@domain.com"
+    blank: [/.+/, 'Email address cannot be left blank'],
+    noAt: [/^[^@]+@/i, 'Email address must contain "@"'],
+    noDomain: [/^[^@]+@[a-z0-9-]+/i, 'Email address must contain domain name, e.g. "gmail.com"'],
+    noSuffix: [/^[^@]+@[a-z0-9-]+\.[a-z]{2,}$/i, 'Email address must have a domain suffix, e.g. ".com"']
+  },
+  expMonth: {
+    blank: [
+      /^\d/,
+      'Credit card expiration date is required.'
+    ]
+  },
+  expYear: {
+    blank: [
+      /^\d/,
+      'Credit card expiration year is required.'
+    ]
   },
   ccNum: {
-    blank: 'Credit card number cannot be blank',
-    notNumber: 'Only numeric characters allowed',
-    tooShort: 'Credit card number must be at least 13 digits',
-    tooLong: 'Credit card number can be no more than 16 digits',
-    zero: 'All zeroes is not a valid credit card number'
+    blank: [
+      /.+/,
+      'Credit card number cannot be left blank.'
+    ],
+    nan: [
+      /^\d+$/,
+      'Only numeric characters allowed.'
+    ],
+    length: [
+      /^\d{13,16}$/,
+      'Credit card number must be between 13 - 16 digits'
+    ]
   },
   zip: {
-    blank: 'Zip code must be 5 digits',
-    notNumber: 'Only numeric characters allowed',
-    zero: 'All zeroes is not a valid zip code'
-  }, 
+    blank: [
+      /.+/,
+      'Zip code cannot be left blank.'
+    ],
+    nan: [
+      /^\d+$/,
+      'Only numeric characters allowed.'
+    ],
+    length: [
+      /^\d{5}$/,
+      'Zip code must be 5 digits.'
+    ]
+  },
   cvv: {
-    blank: 'CVV must be 3 digits',
-    notNumber: 'Only numeric characters allowed',
-    zero: 'All zeroes is not a valid CVV'
+    blank: [
+      /.+/,
+      'CVV cannot be left blank.'
+    ],
+    nan: [
+      /^\d+$/,
+      'Only numeric characters allowed.'
+    ],
+    length: [
+      /^\d{3}$/,
+      'CVV must be 3 digits.'
+    ]
   }
 }
 
-nameInput.addEventListener('blur', () => {
-  const hintSpan = document.getElementById('name-hint');
-  if (validateText('name') === 'valid') {
-    hintSpan.style.display = 'none';
+  // validation function
+  // returns an array: [passed, error]
+  // passed = boolean
+  // error = hint message
+function validate(inputId) {
+  const inputElement = document.getElementById(inputId);
+  const inputValue = inputElement.value;
+  let passed = false;
+  let error;
+  if (inputValue) {
+    for (let toTest in tests[camelify(inputId)]) {
+      console.log(toTest);
+      if (tests[camelify(inputId)][toTest][0].test(inputValue)) {
+        passed = true;
+      } else {
+        passed = false;
+        error = tests[camelify(inputId)][toTest][1];
+        break;
+      }
+    }
   } else {
-    hintSpan.style.display = 'inline';
+    error = tests[camelify(inputId)].blank[1];
   }
-});
-nameInput.addEventListener('keyup', () => {
-  const hintSpan = document.getElementById('name-hint');
-  if (validateText('name') === 'valid') {
-    hintSpan.style.display = 'none';
-  } else {
-    hintSpan.style.display = 'inline';
-  }
-});
+  return [passed, error];
+}
 
-emailInput.addEventListener('blur', () => {
-  const hintSpan = document.getElementById('email-hint');
-  if (validateText('email') === 'valid') {
-    hintSpan.style.display = 'none';
-  } else {
-    hintSpan.style.display = 'inline';
-    hintSpan.innerHTML = hintMsgs.email[validateText('email')];
-  }
-});
-emailInput.addEventListener('keyup', () => {
-  const hintSpan = document.getElementById('email-hint');
-  if (validateText('email') === 'valid') {
-    hintSpan.style.display = 'none';
-  } else {
-    hintSpan.style.display = 'inline';
-    hintSpan.innerHTML = hintMsgs.email[validateText('email')];
-  }
-});
+form.addEventListener('submit', e => {
+  // e.preventDefault(); // delete this
 
-ccNumInput.addEventListener('blur', () => {
-  const hintSpan = document.getElementById('cc-hint');
-  if (validateText('cc-num') === 'valid') {
-    hintSpan.style.display = 'none';
-  } else {
-    hintSpan.style.display = 'inline';
-    hintSpan.innerHTML = hintMsgs.ccNum[validateText('cc-num')];
-  }
-});
-ccNumInput.addEventListener('keyup', () => {
-  const hintSpan = document.getElementById('cc-hint');
-  if (validateText('cc-num') === 'valid') {
-    hintSpan.style.display = 'none';
-  } else {
-    hintSpan.style.display = 'inline';
-    hintSpan.innerHTML = hintMsgs.ccNum[validateText('cc-num')];
-  }
-});
+  const idsToTest = [
+    'name',
+    'email',
+    'exp-month',
+    'exp-year',
+    'cc-num',
+    'zip',
+    'cvv'
+  ];
 
-zipInput.addEventListener('keyup', () => {
-  const hintSpan = document.getElementById('zip-hint');
-  if (validateText('zip') === 'valid') {
-    hintSpan.style.display = 'none';
-  } else {
-    hintSpan.style.display = 'inline';
-    hintSpan.innerHTML = hintMsgs.zip[validateText('zip')];
-  }
-});
-zipInput.addEventListener('blur', () => {
-  const hintSpan = document.getElementById('zip-hint');
-  if (validateText('zip') === 'valid') {
-    hintSpan.style.display = 'none';
-  } else {
-    hintSpan.style.display = 'inline';
-    hintSpan.innerHTML = hintMsgs.zip[validateText('zip')];
-  }
-});
+  for (let id of idsToTest) {
+    // break loop on credit card info
+    // if credit card not selected
+    if (id === 'exp-month' && payMethod.value !== 'credit-card') {
+      break;
+    }
+    
+    const inputElement = document.getElementById(id);
+    const hintSpan = id === 'cc-num' ?
+      document.getElementById('cc-hint') :
+      document.getElementById(id + '-hint');
+    console.log (`line 230 ${id} hintspan`, hintSpan);
 
 
-cvvInput.addEventListener('keyup', () => {
-  const hintSpan = document.getElementById('cvv-hint');
-  if (validateText('cvv') === 'valid') {
-    hintSpan.style.display = 'none';
-  } else {
-    hintSpan.style.display = 'inline';
-    hintSpan.innerHTML = hintMsgs.cvv[validateText('cvv')];
+    if (!validate(id)[0]) {
+      e.preventDefault();
+      console.log(id, 'failed');
+
+      inputElement.parentElement.classList.add('not-valid');
+      inputElement.parentElement.classList.remove('valid');
+      
+      if (hintSpan && hintSpan !== undefined) {
+        console.log('hintSpan:', hintSpan);
+        hintSpan.style.display = 'block';
+        hintSpan.innerHTML = validate(id)[1];
+      }
+    } else {
+      console.log(id, 'passed');
+      inputElement.parentElement.classList.add('valid');
+      inputElement.parentElement.classList.remove('not-valid');
+      // hide hint message
+      if (hintSpan) {
+        hintSpan.style.display = 'none';
+      }
+    }
   }
-});
-cvvInput.addEventListener('blur', () => {
-  const hintSpan = document.getElementById('cvv-hint');
-  if (validateText('cvv') === 'valid') {
-    hintSpan.style.display = 'none';
+
+  if (totalCost === 0) {
+    e.preventDefault();
+    activitiesBox.parentElement.classList.add('not-valid');
+    activitiesBox.parentElement.classList.remove('valid');
+    activitiesBox.parentElement.lastElementChild.style.display = 'block';
   } else {
-    hintSpan.style.display = 'inline';
-    hintSpan.innerHTML = hintMsgs.cvv[validateText('cvv')];
+    activitiesBox.parentElement.classList.add('valid');
+    activitiesBox.parentElement.classList.remove('not-valid');
+    activitiesBox.parentElement.lastElementChild.style.display = 'none';
   }
 });
 
 
-// function to convert id's containing '-' to camel case
+// convert id's containing '-' to camel case
 function camelify(text) {
   const arr = text.split('-');
   if (arr.length === 1) {
@@ -311,5 +289,4 @@ function camelify(text) {
   }
   return camel;
 }
-
 
